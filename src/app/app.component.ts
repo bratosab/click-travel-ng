4e741782363f6d3db14cdcb2781c0a3c2b4412d5
Abdallah Bratos <bratosab@gmail.com>
2020-11-24 23:56:02 +0100

    feat(home page): created basic landing page with destinations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'click-travel-ng';
+  title = 'Choose your dream destination...';
 }
